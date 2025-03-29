1a0248f0eb2d02af0f477196025991ebf142252c
david <david.andrei.batca@gmail.com>
2025-03-29 18:20:02 +0200

    first commit

---
+++ server/drizzle/migrations/0000_nostalgic_dark_beast.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "students" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"age" integer NOT NULL
+);
--- server/drizzle/schema/students.ts
+++ server/drizzle/schema/students.ts
@@ -1,7 +1,7 @@
-import { integer, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
-export const students = {
+export const students = pgTable("students", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
   age: integer("age").notNull(),
-};
+});
