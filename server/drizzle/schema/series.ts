51a0d5039e152d16719f58c2f7182ca63140f2c0
david <david.andrei.batca@gmail.com>
2025-04-05 02:07:35 +0300

    adaugare campuri noi pentru tabele din baza de date si inceperea structurarii relatiilor dintre tabele

--- server/drizzle/schema/series.ts
+++ server/drizzle/schema/series.ts
@@ -1,6 +1,10 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, varchar } from "drizzle-orm/pg-core";
 
-export const series = pgTable("series", {
+export const Series = pgTable("series", {
   id: serial().primaryKey(),
   name: varchar({ length: 3 }).notNull().unique(),
+  groupsYear1: integer().notNull(),
+  groupsYear2: integer().notNull(),
+  groupsYear3: integer().notNull(),
+  groupsYear4: integer().notNull(),
 });
--- server/drizzle/schema/students.ts
+++ server/drizzle/schema/students.ts
@@ -1,9 +1,11 @@
 import { integer, pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { Groups } from "./groups";
 
-export const students = pgTable("students", {
+export const Students = pgTable("students", {
   id: serial().primaryKey(),
   name: text().notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   phone: varchar({ length: 12 }).notNull(),
+  groupId: integer("group_id").references(() => Groups.id),
 });
